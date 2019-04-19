506ba22dfa9eefe9cf87fc6f1392831c48051651
AnatoliyKhaulin <haulin@e-krit.ru>
2019-04-19 11:21:58 +0300

    fl-lac

--- flic-flac/js/main.js
+++ flic-flac/js/main.js
@@ -186,6 +186,114 @@ if (/Mobi/.test(navigator.userAgent)) {
 
 /***/ }),
 
+/***/ "./src/js/contacts-map.js":
+/*!********************************!*\
+  !*** ./src/js/contacts-map.js ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var adressCordOne = [59.844050, 30.386979];
+var adressCordTwo = [60.026132, 30.317916];
+var adressCordThree = [59.940271, 30.265374];
+var contactsAdressOne = document.querySelector('#contactsAdressOne');
+var contactsAdressTwo = document.querySelector('#contactsAdressTwo');
+var contactsAdressThree = document.querySelector('#contactsAdressThree');
+
+function init() {
+  // Карта первая вкладка
+  var contactsMap = new ymaps.Map("contactsMap", {
+    center: adressCordOne,
+    zoom: 9
+  }, {
+    searchControlProvider: 'yandex#search'
+  });
+  contactsMap.controls.add(new ymaps.control.ZoomControl({
+    options: {
+      position: {
+        left: 10,
+        bottom: 70
+      }
+    }
+  }));
+  onePlacemark = new ymaps.Placemark(adressCordOne, {
+    hintContent: '«ФЛИК ФЛЯК» КУПЧИНО:',
+    balloonContent: '192284, Санкт-Петербург, ул. Купчинская, 4 (корп. 4)'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#image',
+    // Своё изображение иконки метки.
+    iconImageHref: './images/sprites/svg/flac_location_map.svg',
+    // Размеры метки.
+    iconImageSize: [123, 63],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
+    iconImageOffset: [-5, -38]
+  });
+  twoPlacemark = new ymaps.Placemark(adressCordTwo, {
+    hintContent: '«ФЛИК ФЛЯК» ОЗЕРКИ:',
+    balloonContent: '192284, Санкт-Петербург, пр. Энгельса 98'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#image',
+    // Своё изображение иконки метки.
+    iconImageHref: './images/sprites/svg/flac_location_map.svg',
+    // Размеры метки.
+    iconImageSize: [123, 63],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
+    iconImageOffset: [-5, -38]
+  });
+  threePlacemark = new ymaps.Placemark(adressCordThree, {
+    hintContent: '«ФЛИК ФЛЯК» ВАСИЛЕОСТРОВСКАЯ:',
+    balloonContent: 'метро Василеостровская, 16 линия В.О. д. 45'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#image',
+    // Своё изображение иконки метки.
+    iconImageHref: './images/sprites/svg/flac_location_map.svg',
+    // Размеры метки.
+    iconImageSize: [123, 63],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
+    iconImageOffset: [-5, -38]
+  });
+  contactsMap.geoObjects.add(onePlacemark).add(twoPlacemark).add(threePlacemark);
+  contactsMap.controls.remove('geolocationControl');
+  contactsMap.controls.remove('searchControl');
+  contactsMap.controls.remove('trafficControl');
+  contactsMap.controls.remove('typeSelector');
+  contactsMap.controls.remove('fullscreenControl');
+  contactsMap.controls.remove('rulerControl');
+  contactsMap.controls.remove('zoomControl');
+  contactsMap.behaviors.disable('scrollZoom');
+  contactsMap.behaviors.disable('drag');
+  $(window).on('resize', function () {
+    setTimeout(function () {
+      contactsMap.container.fitToViewport();
+    }, 250);
+  });
+  contactsAdressOne.addEventListener('click', function (e) {
+    e.preventDefault();
+    contactsMap.setCenter(adressCordOne, 17);
+  });
+  contactsAdressTwo.addEventListener('click', function (e) {
+    e.preventDefault();
+    contactsMap.setCenter(adressCordTwo, 17);
+  });
+  contactsAdressThree.addEventListener('click', function (e) {
+    e.preventDefault();
+    contactsMap.setCenter(adressCordThree, 17);
+  });
+}
+
+ymaps.ready(init);
+
+/***/ }),
+
 /***/ "./src/js/dropdown-menu.js":
 /*!*********************************!*\
   !*** ./src/js/dropdown-menu.js ***!
@@ -292,12 +400,15 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _catalog__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./catalog */ "./src/js/catalog.js");
 /* harmony import */ var _catalog__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_catalog__WEBPACK_IMPORTED_MODULE_4__);
 /* harmony import */ var _triple_slider__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./triple-slider */ "./src/js/triple-slider.js");
+/* harmony import */ var _contacts_map__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./contacts-map */ "./src/js/contacts-map.js");
+/* harmony import */ var _contacts_map__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(_contacts_map__WEBPACK_IMPORTED_MODULE_6__);
 
 
 
 
 
- // import './contacts-map';
+
+
 
 /***/ }),
 
